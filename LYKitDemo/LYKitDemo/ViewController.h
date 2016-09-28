625b8e1e621f66126bfe46bddba3942fb368aade
liyang.github.io <995720636@qq.com>
2016-09-28 13:06:11 +0800

    添加了一个LYKit的演示demo

---
+++ LYKitDemo/LYKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYKitDemo
+//
+//  Created by liyang on 16/9/28.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LYKitDemo/LYKitDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LYKitDemo
+//
+//  Created by liyang on 16/9/28.
+//  Copyright © 2016年 liyang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
